8fb89250102ebb0c8897fb705b045031fc5b91dc
Harry Dodson <harry.lee.dodson@gmail.com>
2023-11-06 14:55:10 -0500

    added Spring

---
+++ src/test/java/com/dodson/personalfinance/PersonalfinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dodson.personalfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalfinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
