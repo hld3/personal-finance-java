6d5b7d711d8f10fc686db00918065b1a879a4b1a
Harry Dodson <harry.lee.dodson@gmail.com>
2023-11-20 14:25:34 -0500

    added something for logging.

--- src/main/java/com/dodson/personalfinance/controller/UserController.java
+++ src/main/java/com/dodson/personalfinance/controller/UserController.java
@@ -3,6 +3,8 @@ package com.dodson.personalfinance.controller;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -21,6 +23,7 @@ import jakarta.validation.Valid;
 @RequestMapping("/user")
 public class UserController {
 
+	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 	private UserService userService;
 
 	UserController(UserService userService) {
@@ -34,6 +37,7 @@ public class UserController {
 				.stream()
 				.collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
 
+			logger.error("There was an error with the request: " + errors);
 			return ResponseEntity.badRequest().body(Map.of("errors", errors));
 		}
 
@@ -41,7 +45,7 @@ public class UserController {
 			userService.registerNewUser(user);
 			return ResponseEntity.ok("User registered successfully.");
 		} catch (Exception e) {
-			// TODO logging.
+			logger.error("Error registering new user: " + e.getMessage());
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error registering user: " + e.getMessage());
 		}
 	}
